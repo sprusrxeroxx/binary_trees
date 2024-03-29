8f41d1b48122bb8c742456ca8ca04460cf618fb5
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-03-29 04:25:31 +0200

    output formating checker fail

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,16 +9,22 @@
 
 size_t binary_tree_height(const binary_tree_t *tree) 
 {
-    size_t right_height, left_height;
+    size_t right_height = 0, left_height = 0;
 
-   if (tree == NULL) {
+   if (tree == NULL) 
+   {
        return 0;
    }
-
-   /* Recursively calculate the heights of the left and right subtrees */
-   left_height = binary_tree_height(tree->left);
-   right_height = binary_tree_height(tree->right);
-
+   else
+   {
+    if (tree)
+    {
+        /* Recursively calculate the heights of the left and right subtrees */
+        left_height = binary_tree_height(tree->left);
+        right_height = binary_tree_height(tree->right);
+    }
    /* Return the height of the taller subtree + 1 (for the current node) */
-   return (1 + (left_height > right_height ? left_height : right_height));
+    return (1 + (left_height > right_height ? left_height : right_height));
+   }
+
 }
