7a0e18a12a255f6fd7ee95792beb67800d6c438d
sprusrxeroxx <ntokozovilakazi36@gmail.com>
2024-03-29 04:56:07 +0200

    betty changes + file tweaks

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - function that return the size of a tree
+ * @tree: tree to check
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0, right = 0, left = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		left = binary_tree_size(tree->left);
+		right = binary_tree_size(tree->right);
+		size = right + left + 1;
+	}
+	return (size);
+}
